65a8740899b8d0c6afe6ba65d4c1196973e80e20
Scott Wachal <scott.wachal@gmail.com>
2021-02-11 15:30:16 -0600

    Added Genres and MovieGenres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   belongs_to :production_company
   validates :title, uniqueness: true
   validates :title, :year, :duration, :description, :average_vote, presence: true
